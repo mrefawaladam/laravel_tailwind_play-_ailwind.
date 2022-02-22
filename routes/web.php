3fa5fee2a28f1eab4afba64b9e059521ce50c527
mrefawaladam <aerfa55@gmail.com>
2022-02-23 06:57:58 +0700

    fix themplate & routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
+
+Route::get('/pricing', function(){
+    return view('pricing');
+});
+
+Route::get('/blog-grids', function(){
+    return view('blog-grids');
+});
+
+Route::get('/blog-details', function(){
+    return view('blog-details');
+});
+
+Route::get('/signup', function(){
+    return view('signup');
+});
+
+Route::get('/signin', function(){
+    return view('signin');
+});
+
+Route::get('/404', function(){
+    return view('404');
+});
